1df70c409f6b92d3c146aa6dbbfd28ea99b360c9
lvlindav <lvlindav23@gmail.com>
2021-02-23 21:01:45 -0600

    fixed ip address

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '54.146.224.245',
+      host: '52.201.246.231',
       username: 'ubuntu',
       privateKey: 'riot-key.pem'
     })
